20364e7a47589b411869ef96393e13830ee8bcb2
Ellen Rafferty <ellenrafferty@Ellens-MacBook-Pro-2.local>
2021-11-21 22:05:13 -0800

    adding updates to database

--- queries.sql
+++ queries.sql
@@ -1,27 +1,14 @@
-CREATE TABLE flight_delays (
-	year INT,
-	month INT,
-	day_of_month INT,
-	flight_num INT,
-	origin_airport_id INT,
-	origin_airport_code VARCHAR(3),
-	origin_city_name VARCHAR,
-	origin_state_nm VARCHAR,
-	dest_airport_id INT,
-	dest_airport_code VARCHAR (3),
-	dest_city_name VARCHAR,
-	dest_state_nm VARCHAR,
-	dept_time INT,
-	arr_time INT,
-	arr_delay FLOAT,
-	cancelled FLOAT,
-	air_time FLOAT,
-	flights FLOAT,
-	distance FLOAT,
-	carrier_delay FLOAT,
-	weather_delay FLOAT,
-	NAS_delay FLOAT,
-	security_delay FLOAT,
-	late_aircraft_delay FLOAT
-	);
-	
+SELECT 
+	fd.day_of_week,
+	w.weekday,
+	fd.flight_num,
+	fd.origin_airport_id,
+	oa.airport_code AS origin_airport_code,
+	oa.city_name AS origin_city_name,
+	fd.dest_airport_id,
+	da.airport_code AS dest_airport_code,
+	da.city_name AS dest_city_name,
+FROM flight_delays fd
+LEFT JOIN airports oa on oa.airport_id = fd.origin_airport_id
+LEFT JOIN airports da on da.airport_id = fd.dest_airport_id
+LEFT JOIN weekdays w on w.code = fd.day_of_week
